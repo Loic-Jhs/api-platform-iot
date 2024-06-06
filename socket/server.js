44f8c562e1a86602212d26582339a050d3621a49
Loic Jouhans <loic.jouhans@ixblue.com>
2024-06-06 23:22:47 +0200

    Setting up new topic and conditional data sending (WIP)

--- socket/server.js
+++ socket/server.js
@@ -1,38 +1,58 @@
-var SerialPort = require('serialport');
-var xbee_api = require('xbee-api');
+var SerialPort = require("serialport");
+var xbee_api = require("xbee-api");
 var C = xbee_api.constants;
 
-require('dotenv').config()
-const mqtt = require('mqtt');
-const client = mqtt.connect('mqtt://test.mosquitto.org');
-let lastReceivedMessage = ''; // Garder une trace du dernier message envoyé
+require("dotenv").config();
+const mqtt = require("mqtt");
+const client = mqtt.connect("mqtt://test.mosquitto.org");
+let lastReceivedMessage = ""; // Garder une trace du dernier message envoyé
 
-client.on('connect', function () {
-  console.log('Connected to MQTT broker');
-  
+client.on("connect", function () {
+  console.log("Connected to MQTT broker");
+
   // Subscribe to the topic
-  client.subscribe('test/topic', function (err) {
+  client.subscribe("test/topic", function (err) {
     if (!err) {
-      console.log('Subscribed to topic test/topic');
+      console.log("Subscribed to topic test/topic");
     } else {
-      console.error('Failed to subscribe: ', err);
+      console.error("Failed to subscribe: ", err);
+    }
+  });
+
+  client.subscribe("enterDoor/state", function (err) {
+    if (!err) {
+      console.log("Subscribed to topic enterDoor/state");
+    } else {
+      console.error("Failed to subscribe: ", err);
     }
   });
 });
 
+/** //TODO
+ * Créer un subscriber d'ouverture et de fermeture de barrière
+ * Pour l'envoie des données du photocapteur utiliser => 0x10: ZigBee Transmit Request (ZNet, ZigBee) https://github.com/jankolkmeier/xbee-api?tab=readme-ov-file#0x10-zigbee-transmit-request-znet-zigbee
+ * Faire du conditionnel pour trier les data du photo capteur
+ * Condition d'entré :
+ *   => Si data < ~1200 sur 1er capteur = barrière ouverte
+ *     => Si data < ~1200 sur 2e capteur = barrière fermée && place de parking -1
+ * Condition de sortie :
+ *   => Si data < ~1200 sur 1er capteur = barrière ouverte
+ *     => Si data < ~1200 sur 2e capteur = barrière fermée && place de parking +1
+ */
+
 let isResponding = false; // Drapeau pour indiquer si une réponse est en cours
 
-client.on('message', function (topic, message) {
+client.on("message", function (topic, message) {
   const receivedMessage = message.toString();
   console.log(`Received message: ${receivedMessage} on topic: ${topic}`);
-  
+
   // Vérifier si le message reçu est différent du dernier envoyé
   if (!isResponding && receivedMessage !== lastReceivedMessage) {
     // Activer le drapeau pour indiquer qu'une réponse est en cours
     isResponding = true;
 
     // Publier un message de réponse sur le même sujet
-    client.publish('test/topic', `Received your message: ${receivedMessage}`);
+    client.publish("test/topic", `Received your message: ${receivedMessage}`);
     lastReceivedMessage = receivedMessage; // Mettre à jour le dernier message envoyé
 
     // Désactiver le drapeau après un court délai
@@ -53,22 +73,27 @@ if (!SERIAL_PORT) {
 }
 
 var xbeeAPI = new xbee_api.XBeeAPI({
-  api_mode: 2
+  api_mode: 2,
 });
 
-let serialport = new SerialPort(SERIAL_PORT, {
-  baudRate: parseInt(process.env.SERIAL_BAUDRATE) || 9600,
-}, function (err) {
-  if (err) {
-    return console.log('Error: ', err.message)
+let serialport = new SerialPort(
+  SERIAL_PORT,
+  {
+    baudRate: parseInt(process.env.SERIAL_BAUDRATE) || 9600,
+  },
+  function (err) {
+    if (err) {
+      return console.log("Error: ", err.message);
+    }
   }
-});
+);
 
 serialport.pipe(xbeeAPI.parser);
 xbeeAPI.builder.pipe(serialport);
 
 serialport.on("open", function () {
-  var frame_obj = { // AT Request to be sent
+  var frame_obj = {
+    // AT Request to be sent
     type: C.FRAME_TYPE.AT_COMMAND,
     command: "NI",
     commandParameter: [],
@@ -76,14 +101,14 @@ serialport.on("open", function () {
 
   xbeeAPI.builder.write(frame_obj);
 
-  frame_obj = { // AT Request to be sent
+  frame_obj = {
+    // AT Request to be sent
     type: C.FRAME_TYPE.REMOTE_AT_COMMAND_REQUEST,
     destination64: "FFFFFFFFFFFFFFFF",
     command: "NI",
     commandParameter: [],
   };
   xbeeAPI.builder.write(frame_obj);
-
 });
 
 // All frames parsed by the XBee will be emitted here
@@ -92,24 +117,42 @@ xbeeAPI.parser.on("data", function (frame) {
     console.log("C.FRAME_TYPE.ZIGBEE_RECEIVE_PACKET");
     let dataReceived = String.fromCharCode.apply(null, frame.data);
     console.log(">> ZIGBEE_RECEIVE_PACKET >", dataReceived);
-
   } else if (C.FRAME_TYPE.NODE_IDENTIFICATION === frame.type) {
     console.log("NODE_IDENTIFICATION");
+    console.log(frame);
     // Handle node identification
     // storage.registerSensor(frame.remote64)
-
   } else if (C.FRAME_TYPE.ZIGBEE_IO_DATA_SAMPLE_RX === frame.type) {
-    console.log("ZIGBEE_IO_DATA_SAMPLE_RX")
-    console.log(frame.analogSamples.AD0)
+    console.log("ZIGBEE_IO_DATA_SAMPLE_RX");
+    //console.log(frame.analogSamples.AD0);
+
+    if (frame.analogSamples.AD0 < 800) {
+      // envoi d'instruction d'ouverture 0x10: ZigBee Transmit Request
+      let frame_obj = {};
+      frame_obj = {
+        type: 0x10, // xbee_api.constants.FRAME_TYPE.ZIGBEE_TRANSMIT_REQUEST
+        destination64: "", //Envoyer l'adresse MAC en dynamique
+        data: "TxData0A", // Can either be string or byte array.
+      };
+    } else if (frame.analogSamples.AD > 800) {
+      // envoi d'instruction de fermeture 0x10: ZigBee Transmit Request
+      let frame_obj = {};
+      frame_obj = {
+        type: 0x10, // xbee_api.constants.FRAME_TYPE.ZIGBEE_TRANSMIT_REQUEST
+        destination64: "", //Envoyer l'adresse MAC en dynamique
+        data: "TxData0A", // Can either be string or byte array.
+      };
+    }
+    client.publish("enterDoor/state", "" + frame.analogSamples.AD0);
+
     // Handle I/O data sample
     // storage.registerSample(frame.remote64, frame.analogSamples.AD0)
-
   } else if (C.FRAME_TYPE.REMOTE_COMMAND_RESPONSE === frame.type) {
-    console.log("REMOTE_COMMAND_RESPONSE")
+    console.log("REMOTE_COMMAND_RESPONSE");
   } else {
     console.debug(frame);
     if (frame.commandData) {
-      let dataReceived = String.fromCharCode.apply(null, frame.commandData)
+      let dataReceived = String.fromCharCode.apply(null, frame.commandData);
       console.log(dataReceived);
     }
   }
